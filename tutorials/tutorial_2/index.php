dc7e3618978c6689fc51b901d8d154c8658a9494
Phyo Phyo Aung <pepeag.1995@gmail.com>
2022-02-16 15:01:39 +0630

    [master]finished php tutorial_2 and review update

--- tutorials/tutorial_2/index.php
+++ tutorials/tutorial_2/index.php
@@ -2,54 +2,48 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Diamond Tutorial</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Diamond Tutorial</title>
 </head>
 <style>
 .container {
-  width: 400px;
-  margin: 0 auto;
-  text-align: center;
+    width: 400px;
+    margin: 0 auto;
+    text-align: center;
 }
 
 h1 {
-  text-align: center;
-  margin: 30px 0;
+    text-align: center;
+    margin: 30px 0;
 }
 </style>
 
 <body class="container">
-  <h1>Diamond Tutorial</h1>
-  <?php $n=6;
+    <h1>Diamond Tutorial</h1>
+    <?php $n = 6;
 
-for ($row=1; $row <=$n; $row++) {
-  for ($i=1; $i <=(2 * $n) - 1; $i++) {
-    if ($i >=$n - ($row - 1) && $i <=$n + ($row - 1)) {
-      echo "* ";
+for ($row = 1; $row <= $n; $row++) {
+    for ($i = 1; $i <= (2 * $n) - 1; $i++) {
+        if ($i >= $n - ($row - 1) && $i <= $n + ($row - 1)) {
+            echo "* ";
+        } else {
+            echo " &nbsp;&nbsp; ";
+        }
     }
-
-    else {
-      echo " &nbsp;&nbsp; ";
-    }
-  }
-
-  echo "<br/>";
+    echo "<br/>";
 }
 
-for ($row=($n - 1); $row >=1; $row--) {
-  for ($i=1; $i <=(2 * $n) - 1; $i++) {
-    if ($i >=$n - ($row - 1) && $i <=$n + ($row - 1)) {
-      echo "* ";
+for ($row = ($n - 1); $row >= 1; $row--) {
+    for ($i = 1; $i <= (2 * $n) - 1; $i++) {
+        if ($i >= $n - ($row - 1) && $i <= $n + ($row - 1)) {
+            echo "* ";
+        } else {
+            echo " &nbsp;&nbsp; ";
+        }
     }
-
-    else {
-      echo " &nbsp;&nbsp; ";
-    }
-  }
-
-  echo "<br/>";
+    echo "<br/>";
 }
 
 ?>
