54bcf9672717e8238010579b467a18e9508630a4
Phyo Phyo Aung <pepeag.1995@gmail.com>
2022-02-21 11:47:34 +0630

    [master] tutorial_6 updated

--- tutorials/tutorial_6/upload.php
+++ tutorials/tutorial_6/upload.php
@@ -6,15 +6,15 @@ if (!is_dir($upload_dir)) {
 }
 
 if ($_FILES['userfile']['error'] !== UPLOAD_ERR_OK) {
-    die("Upload failed with error code " . $_FILES['userfile']['error']);
+    die('<center><h2 style="color:red">Upload failed with error code! </h2></center>' . $_FILES['userfile']['error']);
 }
 
 $info = getimagesize($_FILES['userfile']['tmp_name']);
 if ($info === FALSE) {
-    die("Unable to determine image type of uploaded file");
+    die('<center><h2 style="color:red">Unable to determine image type of uploaded file!</h2></center>');
 }
 if (($info[2] !== IMAGETYPE_GIF) && ($info[2] !== IMAGETYPE_JPEG) && ($info[2] !== IMAGETYPE_PNG)) {
-    die("Not a gif/jpeg/png");
+    die('<center><h2 style="color:red">Not a gif/jpeg/png!</h2></center>');
 }
 $upload_file = $upload_dir . "/" . basename($_FILES['userfile']['name']);
 echo "<center><h2>Image Upload Successfully.</h2></center>";
